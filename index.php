ce12c8e57199e8eedab87217071524967fefd6b0
Renato Pitanga <renato.pitanga@gmail.com>
2020-02-21 20:17:25 -0300

    Criação da Classe Usuário

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 
 require_once("config.php");
 
+/*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios;");
-
 echo json_encode($usuarios);
+*/
+
+$root = new Usuario();
+$root->loadByID(25);
+echo $root;
 
 ?>
